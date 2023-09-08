361cf50c7bdf03f0c0558a36f78dba06aedbcddb
Ramish Afaq <ramish881@gmail.com>
2023-09-08 21:51:08 +0500

    adding for pssword page

--- app/src/main/java/com/Ramish/f200433/ForgotPasswordActivity.java
+++ app/src/main/java/com/Ramish/f200433/ForgotPasswordActivity.java
@@ -6,18 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class ForgotPasswordActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.forgot_password);
     }
-
-    public void OpenForgotPassword(View view){
+    public void Homepage(View view){
         Intent intent=new Intent(
-                MainActivity.this,
-                ForgotPasswordActivity.class);
+                ForgotPasswordActivity.this,
+                MainActivity.class);
         startActivity(intent);
     }
 }
