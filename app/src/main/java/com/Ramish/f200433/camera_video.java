2a79038ac0f0cd244601d9cac2a0436d43bb0fd5
Ramish Afaq <ramish881@gmail.com>
2023-09-09 23:36:30 +0500

    adding camera video page

---
+++ app/src/main/java/com/Ramish/f200433/camera_video.java
@@ -0,0 +1,14 @@
+package com.Ramish.f200433;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class camera_video extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera_video);
+    }
+}
