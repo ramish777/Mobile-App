2dc813b2ef113b581842b4c697321c62913a5aa1
Ramish Afaq <ramish881@gmail.com>
2023-09-10 01:09:18 +0500

    adding audiocall page

---
+++ app/src/main/java/com/Ramish/f200433/audio_call.java
@@ -0,0 +1,14 @@
+package com.Ramish.f200433;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class audio_call extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_audio_call);
+    }
+}
