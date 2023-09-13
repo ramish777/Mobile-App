4801ac87b7710afbc3e66948d8b835d7d5175451
Ramish Afaq <ramish881@gmail.com>
2023-09-13 12:14:42 +0500

    adding splash page

---
+++ app/src/main/java/com/Ramish/f200433/splash_msg.java
@@ -0,0 +1,14 @@
+package com.ramish.f200433_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class splash_msg extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_msg);
+    }
+}
